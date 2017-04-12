cb676f57c03426930ea591d8683814411336284e
Olivier Audard <audard@gmail.com>
2017-04-13 00:27:31 +0200

    coffee2js: Convert styles.js to JS

--- src/styles.js
+++ src/styles.js
@@ -1,55 +1,71 @@
-'use strict'
+import Store from './store';
+import { extend } from './utils';
 
-Store  = require './store'
-extend = require('./utils').extend
+// Create store to save styles
+const store = new Store;
 
-# Create store to save styles
-store = new Store
-
-# Default properties for styles
-defaults =
-  'border-radius': '2px'
-  'padding'      : '1px 3px'
-  'margin'       : '0 1px'
+// Default properties for styles
+let defaults = {
+  'border-radius': '2px',
+  'padding'      : '1px 3px',
+  'margin'       : '0 1px',
   'color'        : 'white'
+};
 
-styles =
-  # define a new style or list existing ones
-  style: (name, style) ->
-    if name? and style?
-      style = extend {}, defaults, style
-      store.add name, style
-      return
-    else if name?
-      store.get name
-    else
-      store.list()
+const styles = {
+  // define a new style or list existing ones
+  style(name, style) {
+    if (name != null && style != null) {
+      style = extend({}, defaults, style);
+      store.add(name, style);
+    }
+    else if (name != null) {
+      return store.get(name);
+    }
+    return store.list();
+  },
 
-  defaults: (style) ->
-    defaults = style if style?
-    defaults
+  defaults(style) {
+    if (style != null) {
+      defaults = style;
+    }
+    return defaults;
+  },
 
-  stringForStyle: (name) ->
-    style = store.get name
-    (("#{k}:#{v};" if style.hasOwnProperty k) for k, v of style).join ''
-
-
-# define a few styles
-
-black = 'color': 'black'
-
-styles.style 'black',   extend {}, 'background': 'black'
-styles.style 'blue',    extend {}, 'background': 'blue'
-styles.style 'brown',   extend {}, 'background': 'brown'
-styles.style 'gray',    extend {}, 'background': 'gray'
-styles.style 'green',   extend {}, 'background': 'green'
-styles.style 'purple',  extend {}, 'background': 'purple'
-styles.style 'red',     extend {}, 'background': 'red'
-styles.style 'cyan',    extend {}, black, 'background': 'cyan'
-styles.style 'magenta', extend {}, black, 'background': 'magenta'
-styles.style 'orange',  extend {}, black, 'background': 'orange'
-styles.style 'pink',    extend {}, black, 'background': 'pink'
-styles.style 'yellow',  extend {}, black, 'background': 'yellow'
+  stringForStyle(name) {
+    const style = store.get(name);
+    return ((() => {
+      const result = [];
+      for (const k in style) {
+        const v = style[k];
+        let item;
+        if (style.hasOwnProperty(k)) {
+          item = `${k}:${v};`;
+        }
+        result.push(item);
+      }
+      return result;
+    })()).join('');
+  }
+};
 
 
-module.exports = styles
+// define a few styles
+
+const black = {'color': 'black'};
+
+styles.style('black',   extend({}, {'background': 'black'}));
+styles.style('blue',    extend({}, {'background': 'blue'}));
+styles.style('brown',   extend({}, {'background': 'brown'}));
+styles.style('gray',    extend({}, {'background': 'gray'}));
+styles.style('green',   extend({}, {'background': 'green'}));
+styles.style('purple',  extend({}, {'background': 'purple'}));
+styles.style('red',     extend({}, {'background': 'red'}));
+styles.style('cyan',    extend({}, black, {'background': 'cyan'}));
+styles.style('magenta', extend({}, black, {'background': 'magenta'}));
+styles.style('orange',  extend({}, black, {'background': 'orange'}));
+styles.style('pink',    extend({}, black, {'background': 'pink'}));
+styles.style('yellow',  extend({}, black, {'background': 'yellow'}));
+
+
+export default styles;
