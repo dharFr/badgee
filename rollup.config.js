40ee70e1dab8fec888b309a1bba3ecac67c18a93
Olivier Audard <audard@gmail.com>
2017-07-16 23:36:52 +0200

    Fix node env

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,10 @@ export default {
   sourceMap: true,
   entry: 'src/badgee.js',
   moduleContext: {
-    'src/console.js': 'window'
+    'src/console.js': '(typeof window !== "undefined" ? window : global)'
+    // The line below also works (and results in a smaller bundle) but breaks the tests
+    // suite and I didn't find any workarround yet... PR welcome ^^
+    // 'src/console.js': 'this'
   },
   plugins: [
     buble()
